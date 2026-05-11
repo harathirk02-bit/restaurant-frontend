f4943259a0378bac37bf752d07d17259d8f4bd9a
Deekshitha <deekshitha@gmail.com>
2026-05-11 11:52:46 +0530

    solved

--- jwt.py
+++ jwt.py
@@ -1,16 +1,10 @@
 from datetime import datetime, timedelta
 from jose import jwt
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+SECRET_KEY = "restaurantsecretkey"
+ALGORITHM = "HS256"
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-
-ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-)
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data: dict):
 
