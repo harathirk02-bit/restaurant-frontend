a09d78d1554115e78ad261df3727f6bd85ed3629
bsnandhini13 <bsnandhini221@gmail.com>
2026-05-11 13:54:02 +0530

    solved

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://restaurant-management-system-k8a1.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((config) => {
